1ce5eb08079489b591048c953755ac8ff2da1515
jorgeavargasm <88695956+jorgeavargasm@users.noreply.github.com>
2022-06-16 23:48:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,8 @@ var config = {
 };
     axios({
     method: 'get',
-    url: 'https://ijxswbp5jh.execute-api.us-east-2.amazonaws.com/dev/v1/get-allowed-tokens-to-swap/0xb1d6285a525f82abff5e1136ee5bb7ea7f0b3ecb8940de4bd5074fabca9ea6b711acc685de139ce33b41966680494c0ca21c98252ecce977fed90546c59c359f1c/0x4666E4f3f4A00D06c918A188378Dfd450a210333',
+    //url: 'https://ijxswbp5jh.execute-api.us-east-2.amazonaws.com/dev/v1/get-allowed-tokens-to-swap/0xb1d6285a525f82abff5e1136ee5bb7ea7f0b3ecb8940de4bd5074fabca9ea6b711acc685de139ce33b41966680494c0ca21c98252ecce977fed90546c59c359f1c/0x4666E4f3f4A00D06c918A188378Dfd450a210333',
+    url: 'https://44ifqtwwg27bjskb7iblzxpmpu0zpncz.lambda-url.us-east-2.on.aws/',
     responseType: 'text',
     },config)
     .then(function (response) {
