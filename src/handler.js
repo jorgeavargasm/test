6716200c3c43fde80a747db12da604158a757bb6
Jorge Vargas <jorgeavargasm@hotmail.es>
2022-07-21 17:13:35 +0000

    test commit 2

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports.generateRandomNumber = (event) => {
+module.exports.testFunction = (event) => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("the Random generated integer is: ", randomNumber);
   return randomNumber;
